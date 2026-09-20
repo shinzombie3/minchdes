6bfe9c2683f3837e14bc112d18db2a9099adec44
irori <minchdes@gmail.com>
2026-09-20 14:28:21 +0900

    add config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://shinzombie3.github.io/minchdes',
+    base: '/minchdes',
+});
